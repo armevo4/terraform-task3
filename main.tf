9cc8bdd1fe30e73b509d4f399a1c8af9f7a5aef1
Krikor Garabed Kafalian <k.kafalian@gmail.com>
2022-04-21 15:07:25 +0400

    ec2 test

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_default_vpc" "default" {
 }
 
 resource "aws_default_subnet" "default_az1" {
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "tf-Default-subnet"
